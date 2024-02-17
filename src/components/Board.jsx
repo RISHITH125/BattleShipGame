a5736ebdc993f7b63b111887dfea673d82a7122c
RISHITH125 <rishithp125@gmail.com>
2024-02-18 01:44:22 +0530

    connected Board comp to server

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,32 @@
 import PlayerOne from './PlayerOne';
 import PlayerTwo from './PlayerTwo';
 import { AppContext } from "../App.jsx";
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
+
 function Board() {
-  const { username, socket } = useContext(AppContext);
+  const { socket } = useContext(AppContext);
+  const [P1UserName, setP1UserName] = useState('');
+  const [P2UserName, setP2UserName] = useState('');
+
+  useEffect(() => {
+    const handleMessage = (event) => {
+      const message = JSON.parse(event.data);
+      switch (message.action) {
+        case "BroadCastName":
+          setP1UserName(message.player1);
+          setP2UserName(message.player2);
+          break;
+        default:
+          console.log('Unknown action:', message.action);
+      }
+    };
+
+    socket.addEventListener('message', handleMessage);
+    return () => {
+      socket.removeEventListener('message', handleMessage);
+    };
+  }, [socket]);
+
   return (
     <>
       <div className="monitor h-screen w-full flex flex-col p-1 gap-1">
@@ -13,8 +36,8 @@ function Board() {
         </div>
         {/* second segment */}
         <div className="relative flex flex-1">
-          <PlayerOne username={username} socket={socket}/>
-          <PlayerTwo/>
+          <PlayerOne P1UserName={P1UserName} socket={socket}/>
+          <PlayerTwo P2UserName={P2UserName} socket={socket}/>
         </div>
       </div>
     </>
